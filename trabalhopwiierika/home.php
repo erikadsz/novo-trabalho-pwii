7ff421e928cb83463871946d3caa5bb56ec290bb
Érika <128494386+erikadsz@users.noreply.github.com>
2024-04-08 23:34:49 -0300

    Add files via upload

---
+++ trabalhopwiierika/home.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Nanum+Gothic&display=swap" rel="stylesheet">    <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css"/>
+
+
+    <title> PetCare - O serviço de gestão para sua clínica. </title>
+    <link rel="stylesheet" href="styles.css">
+    <script src="scripts.js"></script>
+
+</head>
+<body>
+<menu>
+<ul>
+    <li> <img src="./img/logo2.png" class="top"/> </li>
+    <li class="liOne"> <a href="#"> Início </a> </li>
+
+    <li class="liOne"> <a href="#"> Planos </a> 
+    </li>
+
+    <li class="liOne"> <a href="#"> Sobre </a> 
+    </li>
+    <li class="liOne"> <a href="#"> Assine </a> 
+
+    <li class="sign">
+  <img src="./img/user.png"><a href="#">Faça seu login! </a> 
+
+    </li>
+   
+<!-- <ul class="dropdown">
+    <li> <a href="#"> Agenda </a> </li>
+    <li> <a href="#"> Pacientes </a> </li>
+    <li> <a href="#"> Pagamentos </a> </li>
+        </ul>
+-->
+ </ul>
+
+ </menu> 
+
+ <div class="container-slider">
+    <button id="prev-button"><img src="./img/arrow.png" alt="prev-button"></button>
+    <div class="container-images">
+      <img src="./img/14.png" alt="image1" class="slider on">
+      <img src="./img/15.png" alt="image2" class="slider">
+    </div>
+    <button id="next-button"><img src="./img/arrow.png" alt="next-button"></button>
+  </div>
+  <div id="contentOne">
+    <div class="content">
+      <h1>O que você pode fazer no nutrium?</h1>
+      
+      <div class="card">
+        <div class="contentCard">
+          <div class="card-front">
+            <img src="./img/cd11.png" class="imgCard">
+          </div>
+          <div class="card-back">
+            <p>Agende seus compromissos e consultas com detalhamentos.</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="contentCard">
+          <div class="card-front">
+            <img src="./img/cd12.png" class="imgCard">
+          </div>
+          <div class="card-back">
+            <p>Tenha controle sobre seus pagamentos, salário e gastos organizadamente.</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="contentCard">
+          <div class="card-front">
+            <img src="./img/cd13.png" class="imgCard">
+          </div>
+          <div class="card-back">
+            <p>Registre seus relatórios para cada cliente e faça dietas e cardapios personalizados.</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="card" id="img4">
+        <div class="contentCard" >
+          <div class="card-front">
+            <img src="./img/cd14.png" class="imgCard">
+          </div>
+          <div class="card-back">
+            <p>Tenha acesso às refeições de seuds pacientes diariamente.</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="contentCard">
+          <div class="card-front">
+            <img src="./img/cd15.png" class="imgCard">
+          </div>
+          <div class="card-back">
+            <p>Converse com seus clientes a qualquer momento e maeque consultas online.</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+<div class="content">
+<h1> Conheça nossos planos! </h1>
+
+
+
+
+</div>
+<footer>
+<p>2024  © Todos os direitos reservados | Nutrium </p>
+</footer>
+    <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+</body>
+</html
